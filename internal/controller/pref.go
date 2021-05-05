829d1454843b05ffc4c2d43a0d39c40ab39d3aba
jorge.fuertes <jorge@jorgefuertes.com>
2021-05-05 19:04:15 +0200

    Themeable

--- internal/controller/pref.go
+++ internal/controller/pref.go
@@ -4,20 +4,29 @@ import (
 	"regexp"
 
 	"git.martianoids.com/queru/retroserver/internal/cfg"
+	"git.martianoids.com/queru/retroserver/internal/helper"
 	"git.martianoids.com/queru/retroserver/internal/locale"
 	"github.com/gofiber/fiber/v2"
 )
 
-func langCtrl(app *fiber.App) {
-	g := app.Group("/lang")
-	// change user's locale
-	g.Get("/:lang", func(c *fiber.Ctx) error {
+func prefCtrl(app *fiber.App) {
+	g := app.Group("/pref")
+
+	// change user's language
+	g.Get("/lang/:lang", func(c *fiber.Ctx) error {
 		matched, _ := regexp.MatchString(cfg.AvailableLangReg, c.Params("lang"))
 		if matched {
 			locale.SetUserLang(c, c.Params("lang"))
 		} else {
 			locale.SetUserLang(c, "en")
 		}
+		return c.Redirect(string(c.Context().Referer()), 302)
+	})
+
+	// change user's color
+	g.Get("/color/:color", func(c *fiber.Ctx) error {
+		h := helper.New(c)
+		h.SetUserColor(c.Params("color"))
 		return c.Redirect(string(c.Context().Referer()), 302)
 	})
 }
