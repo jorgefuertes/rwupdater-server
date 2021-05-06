09181b0dcec84e1511b747026bc4093b3ffc7f79
jorge.fuertes <jorge@jorgefuertes.com>
2021-05-06 11:37:35 +0200

    Downloads OK

---
+++ internal/helper/lang.go
@@ -0,0 +1,32 @@
+package helper
+
+import (
+	"html/template"
+
+	"git.martianoids.com/queru/retroserver/internal/locale"
+)
+
+type Lang struct {
+	Abbr   string
+	Active bool
+}
+
+func (l *Lang) Link() string {
+	return "/pref/lang/" + l.Abbr
+}
+
+// SetPageTile - Set the current page title
+func (h *Helper) SetPageTitle(key string) {
+	h.PageTitle = h.T(key)
+	h.IsMain = (key == "menu.main.title")
+}
+
+// T - Translate
+func (h *Helper) T(key string, arg ...interface{}) template.HTML {
+	return locale.T(h.Lang, key, arg...)
+}
+
+// IsLang - True if Lang == param
+func (h *Helper) IsActiveLang(l string) bool {
+	return (h.Lang == l)
+}
