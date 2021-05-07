7f3ea428d6f3a4662c6b78f741921c2f37d94c39
jorge.fuertes <jorge@jorgefuertes.com>
2021-05-07 20:20:20 +0200

    Session storage fix

--- internal/sess/sess.go
+++ internal/sess/sess.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"time"
 
+	"git.martianoids.com/queru/retroserver/internal/cfg"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
 )
@@ -12,8 +13,20 @@ var store *session.Store
 
 func NewStore() {
 	log.Println("Session new")
+	var domain string
+	if cfg.IsDev() {
+		domain = "localhost"
+	} else {
+		domain = "updater.retrowiki.es"
+	}
 	if store == nil {
-		store = session.New(session.Config{Expiration: 8640 * time.Hour})
+		store = session.New(session.Config{
+			Expiration:     8640 * time.Hour,
+			CookieName:     "rw_sess_id",
+			CookieDomain:   domain,
+			CookieSameSite: "Strict",
+			CookieSecure:   true,
+		})
 	}
 }
 
