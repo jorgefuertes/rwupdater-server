a790d71fbdb0aa8fdb32b4055b533e44bd16be91
jorge.fuertes <jorge@jorgefuertes.com>
2021-05-09 11:25:49 +0200

    No sessions

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -1,16 +1,16 @@
 package helper
 
 import (
+	"fmt"
 	"html/template"
 
-	"git.martianoids.com/queru/retroserver/internal/locale"
 	"github.com/gofiber/fiber/v2"
 )
 
+var colors = []string{"white", "green", "blue", "amber"}
+
 // Helper
 type Helper struct {
-	Ctx       *fiber.Ctx
-	Lang      string
 	PageTitle template.HTML
 	Vars      fiber.Map
 	IsMain    bool
@@ -18,27 +18,24 @@ type Helper struct {
 	Latest    string
 	Err       string
 	Colors    []Color
+	Color     string
 	Langs     []Lang
+	Lang      string
 	MyURL     string
 }
 
 // New - Create and return new Helper
 func New(c *fiber.Ctx) Helper {
 	h := new(Helper)
-	h.Ctx = c
-	h.Colors = make([]Color, 0)
-	h.Colors = append(h.Colors,
-		Color{"W", h.IsActiveColor("W")},
-		Color{"G", h.IsActiveColor("G")},
-		Color{"B", h.IsActiveColor("B")},
-		Color{"A", h.IsActiveColor("A")},
-	)
-	h.Lang = locale.GetUserLang(c)
-	h.Langs = make([]Lang, 0)
-	h.Langs = append(h.Langs, Lang{"en", h.IsActiveLang("en")}, Lang{"es", h.IsActiveLang("es")})
+	h.fillLangs(c)
+	h.fillColors(c)
 	// url
 	h.MyURL = c.BaseURL()
 	// free controller vars
 	h.Vars = make(fiber.Map)
 	return *h
+}
+
+func (h *Helper) LinkTo(p string) string {
+	return fmt.Sprintf("/front/%s/%s/%s", h.Lang, h.Color, p)
 }
