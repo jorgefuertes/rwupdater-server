e512fcb1e7deccc62dbf413680059b5785beee22
jorge.fuertes <jorge@jorgefuertes.com>
2021-05-22 12:41:57 +0200

    Trying

--- internal/matomo/matomo.go
+++ internal/matomo/matomo.go
@@ -1,10 +1,7 @@
 package matomo
 
 import (
-	"fmt"
-	"log"
 	"math/rand"
-	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -38,18 +35,18 @@ func NewVisit(c *fiber.Ctx) {
 	v.Agent = c.Get("User-Agent")
 	v.Lang = c.Get("Accept-Language")
 
-	v.send()
+	// go v.send()
 }
 
-func (v *Visit) send() {
-	query := fmt.Sprintf("idsite=%v&rec=%v&action_name=%s&url=%s&_id=%s&rand=%v&apiv=%v"+
-		"&urlref=%s&ua=%s&lang=%s",
-		v.IDSite, v.Rec, v.ActionName, v.URL, v.ID, v.Rand, v.Version,
-		v.Ref, v.Agent, v.Lang,
-	)
+// func (v *Visit) send() {
+// 	query := fmt.Sprintf("idsite=%v&rec=%v&action_name=%s&url=%s&_id=%s&rand=%v&apiv=%v"+
+// 		"&urlref=%s&ua=%s&lang=%s",
+// 		v.IDSite, v.Rec, v.ActionName, v.URL, v.ID, v.Rand, v.Version,
+// 		v.Ref, v.Agent, v.Lang,
+// 	)
 
-	_, err := http.Get(MURL + "?" + query)
-	if err != nil {
-		log.Println("VISIT REC ERROR:", err)
-	}
-}
+// 	_, err := http.Get(MURL + "?" + query)
+// 	if err != nil {
+// 		log.Println("VISIT REC ERROR:", err)
+// 	}
+// }
